fadf6a653bcb2a417a9f4700ffecc3079ae21000
lemon65 <schoettger.aaron@gmail.com>
2020-01-22 20:13:14 -0800

    pin change

--- hw_interface.py
+++ hw_interface.py
@@ -62,7 +62,7 @@ class HWInterface():
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.source_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         while self.poll_source_button:
-            source_button_state = GPIO.input(source_pin)
+            source_button_state = GPIO.input(self.source_pin)
             if not source_button_state:
                 intic.index_source()
                 time.sleep(0.2)
