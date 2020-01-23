e9778f7bcb30ea086ab979e58b52b2937c8d62c5
lemon65 <schoettger.aaron@gmail.com>
2020-01-22 20:00:49 -0800

    Changing the LCD Type

--- hw_interface.py
+++ hw_interface.py
@@ -9,7 +9,7 @@ logger = logging.getLogger('HWI')
 
 class HWInterface():
     def __init__(self):
-        self.pi_lcd = CharLCD('PCF8574', 0x27)
+        self.pi_lcd = CharLCD(numbering_mode=GPIO.BCM)
         self.poll_source_button = False
         self.max_lcd_rows = int(gi.config_data['LCD']['max_lcd_rows'])
         self.max_lcd_elements = int(gi.config_data['LCD']['max_lcd_elements'])
